49f0c65eeaf076c1af5c3ddfa7dafccdae8bd204
Min Han Kyaw <minhankyaw.mh@gmail.com>
2024-05-13 21:39:21 +0700

    finished vue movie db

---
+++ src/views/MovieDetail.vue
@@ -0,0 +1,57 @@
+<template lang="">
+  <div class="movie-detail">
+    <h2>{{ movie.Title }}</h2>
+    <p>{{ movie.Year }}</p>
+    <img :src="movie.Poster" alt="Movie Poster" class="featured-img" />
+    <p>{{ movie.Plot }}</p>
+  </div>
+</template>
+<script>
+import { ref, onBeforeMount } from "vue";
+import { useRoute } from "vue-router";
+import env from "@/env.js";
+export default {
+  setup() {
+    const movie = ref({});
+    const route = useRoute();
+
+    onBeforeMount(() => {
+      fetch(
+        `https://www.omdbapi.com/?apikey=${env.apikey}&i=${route.params.id}&plot=full`
+      )
+        .then((res) => res.json())
+        .then((data) => {
+          movie.value = data;
+          console.log(data);
+        });
+    });
+
+    return {
+      movie,
+      route,
+    };
+  },
+};
+</script>
+<style lang="scss">
+.movie-detail{
+    padding: 16px;
+    h2{
+        color: #FFF;
+        font-size: 28px;
+        font-weight: 600;
+        margin-bottom: 16px;
+
+    }
+    .feature-img{
+        display: block;
+        max-width: 200px;
+        margin-bottom: 16px;
+    }
+    p{
+        color: #FFF;
+        font-size: 18px;
+        line-height: 1.4;
+    }
+}
+</style>
